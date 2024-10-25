466224d586cbd84c8d9e335a4e0391666a333ade
lbreede <67719520+lbreede@users.noreply.github.com>
2024-10-25 16:20:50 -0700

    added tests/

---
+++ tests/test_c_string.py
@@ -0,0 +1,66 @@
+import pytest
+
+from c_string import CString, FromVecWithNulError, NulError
+
+
+def test_new():
+    with pytest.raises(NulError):
+        CString.new("foo\0")
+
+    with pytest.raises(NulError):
+        CString.new("fo\0o")
+
+    with pytest.raises(NulError):
+        CString.new("fo\0o\0")
+
+    assert CString.new("foo") == CString.new(b"foo")
+    assert CString.new(b"foo") == CString.new(bytearray(b"foo"))
+    assert CString.new(bytearray(b"foo")) == CString.new("foo")
+
+
+def test_into_string():
+    assert CString.new(bytearray(b"foo")).into_string() == "foo"
+
+
+def test_into_bytes():
+    assert CString.new("foo").into_bytes() == bytearray(b"foo")
+
+
+def test_into_bytes_with_nul():
+    assert CString.new("foo").into_bytes_with_nul() == bytearray(b"foo\0")
+
+
+def test_as_bytes():
+    c_string_as_bytes = CString.new("foo").as_bytes()
+    assert c_string_as_bytes == b"foo"
+    assert len(c_string_as_bytes) == 3
+
+
+def test_as_bytes_with_nul():
+    assert CString.new("foo").as_bytes_with_nul() == b"foo\0"
+
+
+def test_from_vec_with_nul_unchecked():
+    assert CString.from_vec_with_nul_unchecked(
+        bytearray(b"abc\0")
+    ) == CString.from_vec_unchecked(bytearray(b"abc"))
+
+
+def test_from_vec_with_nul():
+    with pytest.raises(FromVecWithNulError):
+        CString.from_vec_with_nul(bytearray(b"abc"))
+
+    with pytest.raises(FromVecWithNulError):
+        CString.from_vec_with_nul(bytearray(b"ab\0c\0"))
+
+    with pytest.raises(FromVecWithNulError):
+        CString.from_vec_with_nul(bytearray(b"ab\0c"))
+
+    assert CString.from_vec_with_nul(bytearray(b"abc\0")) == CString.new(
+        bytearray(b"abc")
+    )
+    assert CString.from_vec_with_nul(bytearray(b"abc\0")) == CString.new("abc")
+
+
+def test_repr():
+    assert repr(CString.new("foo")) == repr("foo")
