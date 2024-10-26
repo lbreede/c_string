67a8a169fb650466221346b4f94ae2e5974bcb5f
lbreede <67719520+lbreede@users.noreply.github.com>
2024-10-25 18:08:16 -0700

    incorporated Result-types

--- cstring.py
+++ cstring.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from result import Err, Ok, Result
+
 
 class NulError(Exception):
     def __init__(self, nul_position: int, data: bytearray) -> None:
@@ -23,26 +25,26 @@ class CString:
         self._inner = inner
 
     @classmethod
-    def new(cls, t: bytearray | bytes | str) -> CString:
+    def new(cls, t: bytearray | bytes | str) -> Result[CString, NulError]:
         if isinstance(t, bytes):
             t = bytearray(t)
         elif isinstance(t, str):
             t = bytearray(t.encode())
 
         if 0 in t:
-            raise (NulError(t.index(0), t))
-        return cls.from_vec_unchecked(t)
+            return Err(NulError(t.index(0), t))
+        return Ok(cls.from_vec_unchecked(t))
 
     @classmethod
     def from_vec_unchecked(cls, v: bytearray) -> CString:
         v.append(0)
         return cls(v)
 
-    def into_string(self) -> str:
+    def into_string(self) -> Result[str, UnicodeDecodeError]:
         try:
-            return self._inner[:-1].decode()
+            return Ok(self._inner[:-1].decode())
         except UnicodeDecodeError as e:
-            raise IntoStringError() from e
+            return Err(e)
 
     def into_bytes(self) -> bytearray:
         return self.into_bytes_with_nul()[:-1]
@@ -61,14 +63,15 @@ class CString:
         return cls(v)
 
     @classmethod
-    def from_vec_with_nul(cls, v: bytearray) -> CString:
+    def from_vec_with_nul(cls, v: bytearray) -> Result[CString, str]:
+        # TODO: Properly wrap the error in Err
         if v[-1] != 0:
-            raise FromVecWithNulError("the last element must be a nul byte")
+            return Err("FromVecWithNulError")
 
         if v.count(0) > 1:
-            raise FromVecWithNulError("there are more than one nul byte")
+            return Err("FromVecWithNulError")
 
-        return cls(v)
+        return Ok(cls(v))
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, CString):
@@ -76,7 +79,7 @@ class CString:
         return self.as_bytes_with_nul() == other.as_bytes_with_nul()
 
     def __repr__(self) -> str:
-        return repr(self.into_string())
+        return repr(self.into_string().unwrap())
 
 
 def main() -> None:
--- test_cstring.py
+++ test_cstring.py
@@ -1,17 +1,12 @@
-import pytest
+from result import Ok
 
-from cstring import CString, FromVecWithNulError, IntoStringError, NulError
+from cstring import CString
 
 
 def test_new():
-    with pytest.raises(NulError):
-        CString.new("foo\0")
-
-    with pytest.raises(NulError):
-        CString.new("fo\0o")
-
-    with pytest.raises(NulError):
-        CString.new("fo\0o\0")
+    assert CString.new("foo\0").is_err()
+    assert CString.new("fo\0o").is_err()
+    assert CString.new("fo\0o\0").is_err()
 
     assert CString.new("foo") == CString.new(b"foo")
     assert CString.new(b"foo") == CString.new(bytearray(b"foo"))
@@ -19,8 +14,8 @@ def test_new():
 
 
 def test_into_string():
-    c_string = CString.new(bytearray(b"foo"))
-    assert c_string.into_string() == "foo"
+    c_string = CString.new(bytearray(b"foo")).unwrap()
+    assert c_string.into_string() == Ok("foo")
 
     # hacking inner bytearray
     inner = c_string._inner
@@ -28,28 +23,27 @@ def test_into_string():
     inner.append(128)
     inner.append(0)
     c_string._inner = inner
-    with pytest.raises(IntoStringError):
-        c_string.into_string()
+    assert c_string.into_string().is_err()
 
 
 def test_into_bytes():
-    assert CString.new("foo").into_bytes() == bytearray(b"foo")
+    assert CString.new("foo").unwrap().into_bytes() == bytearray(b"foo")
 
 
 def test_into_bytes_with_nul():
-    bytearray_with_nul = CString.new("foo").into_bytes_with_nul()
+    bytearray_with_nul = CString.new("foo").unwrap().into_bytes_with_nul()
     assert bytearray_with_nul == bytearray(b"foo\0")
     assert bytearray_with_nul[-1] == 0
 
 
 def test_as_bytes():
-    c_string_as_bytes = CString.new("foo").as_bytes()
+    c_string_as_bytes = CString.new("foo").unwrap().as_bytes()
     assert c_string_as_bytes == b"foo"
     assert len(c_string_as_bytes) == 3
 
 
 def test_as_bytes_with_nul():
-    bytes_with_nul = CString.new("foo").as_bytes_with_nul()
+    bytes_with_nul = CString.new("foo").unwrap().as_bytes_with_nul()
     assert bytes_with_nul == b"foo\0"
     assert bytes_with_nul[-1] == 0
 
@@ -61,20 +55,16 @@ def test_from_vec_with_nul_unchecked():
 
 
 def test_from_vec_with_nul():
-    with pytest.raises(FromVecWithNulError):
-        CString.from_vec_with_nul(bytearray(b"abc"))
-
-    with pytest.raises(FromVecWithNulError):
-        CString.from_vec_with_nul(bytearray(b"ab\0c\0"))
-
-    with pytest.raises(FromVecWithNulError):
-        CString.from_vec_with_nul(bytearray(b"ab\0c"))
+    assert CString.from_vec_with_nul(bytearray(b"abc")).is_err()
+    assert CString.from_vec_with_nul(bytearray(b"ab\0c\0")).is_err()
+    assert CString.from_vec_with_nul(bytearray(b"ab\0c")).is_err()
 
     assert CString.from_vec_with_nul(bytearray(b"abc\0")) == CString.new(
         bytearray(b"abc")
     )
+
     assert CString.from_vec_with_nul(bytearray(b"abc\0")) == CString.new("abc")
 
 
 def test_repr():
-    assert repr(CString.new("foo")) == repr("foo")
+    assert repr(CString.new("foo").unwrap()) == repr("foo")
